c5a9213cb6b18a470c87eb53017484d323a23f71
Reid Fleishman <reidfleishman5@gmail.com>
2024-11-24 00:12:17 -0500

    wip

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,14 @@
-import flask
 import saml
-from flask_login import LoginManager
-from flask_login import UserMixin, login_user, login_required, current_user
+from flask import Flask, redirect, send_file, render_template
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
 
 import pandas as pd
+import requests
+import io
 
 login_manager = LoginManager()
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 app.config.update({
     'SECRET_KEY': 'soverysecret',
@@ -46,15 +47,40 @@ def on_saml_authenticated(sender, subject, attributes, auth):
     login_user(user)
 
 @app.get("/")
-def hello_world():
-    return flask.render_template('index.html')
+def home():
+    if current_user.is_authenticated:
+        id = current_user.id
+        db = pd.read_csv("db.csv", index_col="id")
+        cards = db.loc[id].astype(int)
+        print(db.loc[id].astype(int))
+        print(cards.index)
+        sem_names = ["Spring 2016", "Spring 2017", "Spring 2018", "Spring 2019", "Spring 2020", "Spring 2021", "Spring 2022", "Spring 2023", "Spring 2024", "Fall 2024"]
+        return render_template('index.html', sem_shortname=cards.index, num_cards=cards.values, sem_names=sem_names)
+    else:
+        return render_template('index.html')
 
-@app.route("/get-cards")
+# @app.route("/get-cards")
+# @login_required
+# def get_cards():
+#     id = current_user.id
+#     db = pd.read_csv("db.csv", index_col="id")
+#     cards = db.loc[id].astype(int)
+#     print(db.loc[id].astype(int))
+#     print(cards.index)
+#     sem_names = ["Spring 2016", "Spring 2017", "Spring 2018", "Spring 2019", "Spring 2020", "Spring 2021", "Spring 2022", "Spring 2023", "Spring 2024", "Fall 2024"]
+#     return render_template('index.html', sem_shortname=cards.index, num_cards=cards.values, sem_names=sem_names)
+
+@app.route("/get-pdf/<sem>/<message_num>")
 @login_required
-def get_cards():
-    id = current_user.id
-    db = pd.read_csv("db.csv", index_col="id")
-    cards = db.loc[id].astype(int)
-    print(db.loc[id].astype(int))
-    year_names = ["Spring 2016", "Spring 2017", "Spring 2018", "Spring 2019", "Spring 2020", "Spring 2021", "Spring 2022", "Spring 2023", "Spring 2024", "Fall 2024"]
-    return flask.render_template('index.html', cards=cards, year_names=year_names)
+def get_pdf(sem, message_num):
+    # if id == current_user.id:
+        url = f'https://cards.cornelllifted.com/pdfs/{sem}/{current_user.id}/{message_num}_2638.pdf'
+        return redirect(url)
+        # req = requests.get(url)
+        # return send_file(
+        #              io.BytesIO(req.content),
+        #             #  attachment_filename='report.pdf',
+        #              mimetype='application/pdf'
+        #        )
+    # else:
+        # return "You are not authorized to view this Lifted message.  If you think this in error, please email lifted@cornell.edu"
